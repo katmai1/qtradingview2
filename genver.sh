3f9bba273c6513157600d56f28ee73960a2a90ce
katmai <katmai.mobil@gmail.com>
2024-04-10 13:39:07 +0200

    fixes

--- genver.sh
+++ genver.sh
@@ -5,3 +5,8 @@ new="#define APP_VERSION \"$1\""
 sed -i "s/$old/$new/g" version.h
 
 # falta configurar gitcon mi github y probar de hacer un commit y un tag
+git describe --tags --abbrev=0
+git add version.h
+git commit -m "Creando version $1..."
+git tag "v$1"
+git push origin "v$1"
