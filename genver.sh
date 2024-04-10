da3f3019b31ecc8dbb0576c0a8fc12f411c939a6
katmai <katmai.mobil@gmail.com>
2024-04-10 19:39:49 +0200

    f

--- genver.sh
+++ genver.sh
@@ -3,11 +3,12 @@
 old="$(grep APP_VERSION version.h)"
 new="#define APP_VERSION \"$1\""
 sed -i "s/$old/$new/g" version.h
-qmake6 && make
+qmake6 -project QTradingview2
+
 
 # falta configurar gitcon mi github y probar de hacer un commit y un tag
-git describe --tags --abbrev=0
-git add version.h
-git commit -m "Creando version $1..."
-git tag "v$1"
-git push origin "v$1"
+# git describe --tags --abbrev=0
+# git add version.h
+# git commit -m "Creando version $1..."
+# git tag "v$1"
+# git push origin "v$1"
