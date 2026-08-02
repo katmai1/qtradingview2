c3b7d889d00a93e42e3b568729774f66f867a952
katmai <katmai.mobil@gmail.com>
2026-08-02 09:16:25 +0200

    fixes

--- resources/qtradingview2-wrapper.sh
+++ resources/qtradingview2-wrapper.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 HERE="/opt/qtradingview2"
 export LD_LIBRARY_PATH="$HERE/lib:$LD_LIBRARY_PATH"
+export QT_PLUGIN_PATH="$HERE/plugins"
+export QT_QPA_PLATFORM_PLUGIN_PATH="$HERE/plugins/platforms"
 export QTWEBENGINEPROCESS_PATH="$HERE/libexec/QtWebEngineProcess"
 export QTWEBENGINE_RESOURCES_PATH="$HERE/resources"
 export QTWEBENGINE_LOCALES_PATH="$HERE/translations/qtwebengine_locales"
